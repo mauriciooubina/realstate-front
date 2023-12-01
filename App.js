cb764f4dcc3885857e13faf3e81dee03fd4e9896
mauriciooubina <mauriciooubina@gmail.com>
2023-12-01 19:50:18 -0300

    add listern for internet connection

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
+import React, { useEffect } from 'react';
 import RootNavigator from './app/navigation/RootNavigator.js';
+import NetInfo from '@react-native-community/netinfo';
+import {ToastAndroid } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      if (!state.isConnected) {
+        ToastAndroid.show('Por favor, revisa tu conexión y vuelve a intentarlo.!', ToastAndroid.LONG);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
       <RootNavigator/>
   )}
