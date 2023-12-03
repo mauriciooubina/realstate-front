42932654ab13e3fce19a6ae2b483d924c4493d82
Rodrigo <rodrigovictoralcorta@gmail.com>
2023-12-03 19:25:18 -0300

    scope removed

--- app/ui/screens/login/LoginScreenUI.jsx
+++ app/ui/screens/login/LoginScreenUI.jsx
@@ -15,7 +15,7 @@ export default LoginScreenUI = () => {
     const [showLoading, setShowLoading] = useState(true);
 
     GoogleSignin.configure({
-        scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+        // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
         webClientId: '273233604040-qj1fvtk8887njppd2bt4i0ned9qbhfne.apps.googleusercontent.com', // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
         offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
     });
